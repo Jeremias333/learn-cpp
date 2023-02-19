0a9d64986eb7384683b458e1c4da793ba0a5ffce
Jeremias <jeremias@pickcells.bio>
2023-02-19 00:15:49 -0300

    feat: progess in course

--- gameHangman.cpp
+++ gameHangman.cpp
@@ -2,23 +2,28 @@
 #include "string"
 #include "map"
 #include "vector"
+#include "fstream"
+#include "ctime"
+#include "cstdlib"
 
 using namespace std;
 
 map<char, bool> hitted;
 vector<char> miss_hits;
-const string SECRET_WORD = "MELANCIA";
+//const string SECRET_WORD = "MELANCIA";
+string secret_word = "MELANCIA";
 char hit;
+ifstream file;
 
-bool letter_exists(char hit, string SECRET_WORD){
+bool letter_exists(char hit, string secret_word){
 
-    //for (int i = 0; i < SECRET_WORD.size(); i++){
-    //    if (hit == SECRET_WORD[i]){
+    //for (int i = 0; i < secret_word.size(); i++){
+    //    if (hit == secret_word[i]){
     //        return true;
     //    }
     //}
 
-    for (char letter : SECRET_WORD){
+    for (char letter : secret_word){
         if (hit == letter){
             return true;
         }
@@ -26,8 +31,8 @@ bool letter_exists(char hit, string SECRET_WORD){
     return false;
 }
 
-bool aint_correct(string SECRET_WORD){
-    for(char letter : SECRET_WORD){
+bool aint_correct(string secret_word){
+    for(char letter : secret_word){
         if(!hitted[letter]){
             return true;
         }
@@ -57,7 +62,7 @@ void print_wrong_hits(){
 }
 
 void print_correct_letters(){
-    for (char letter : SECRET_WORD){
+    for (char letter : secret_word){
         if(hitted[letter]){
             cout << letter << " ";
         }
@@ -76,7 +81,7 @@ void processing_hits(){
     hitted[hit] = true;
     //cout << "O seu chute foi: " << hit << endl;
 
-    if (letter_exists(hit, SECRET_WORD)){
+    if (letter_exists(hit, secret_word)){
         cout << "Você acertou! Seu chute está na palavra." << endl;
     }else{
         cout << "Você errou! Seu chute não está na palavra." << endl;
@@ -86,10 +91,63 @@ void processing_hits(){
     cout << endl;
 }
 
+void print_game_over(){
+    cout << "Game Over!" << endl;
+    cout << "A palavra secreta era: " << secret_word << endl;
+
+    if (aint_correct(secret_word)){
+        cout << "Você perdeu, tente novamente!" << endl;
+    }
+    else{
+        cout << "Parabéns você acertou!" << endl;
+    }
+}
+
+vector<string> read_file(string path="words.txt"){
+    file.open(path);
+
+
+    if (file.is_open()){
+        int qtd_words;
+        vector<string> words_of_file;
+
+        file >> qtd_words;
+
+        //cout << "O Arquivo possui " << qtd_words << " palavras" << endl;
+
+        for (int i = 0; i<qtd_words; i++){
+            string word_read;
+            file >> word_read;
+            //cout << "Na linha " << i << ": " << word_read << endl;
+
+            words_of_file.push_back(word_read);
+        }
+
+        file.close();
+        return words_of_file;
+    }else{
+        cout << "Não foi possível acessar o banco de palavras." << endl;
+        exit(0);
+    }
+}
+
+void random_word(){
+    vector<string> words = read_file("words.txt");
+
+    srand(time(NULL));
+    int index_rand = rand() % words.size();
+
+    secret_word = words[index_rand];
+}
+
 int main(){
     print_header();
 
-    while(aint_correct(SECRET_WORD) && didnt_hang()){
+    random_word();
+
+    cout << endl;
+
+    while(aint_correct(secret_word) && didnt_hang()){
 
         print_wrong_hits();
 
@@ -98,13 +156,5 @@ int main(){
         processing_hits(); 
     }
 
-    cout << "Game Over!" << endl;
-    cout << "A palavra secreta era: " << SECRET_WORD << endl;
-
-    if (aint_correct(SECRET_WORD)){
-        cout << "Você perdeu, tente novamente!" << endl;
-    }
-    else{
-        cout << "Parabéns você acertou!" << endl;
-    }
+    print_game_over();
 }
